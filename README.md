0929cbca9d905d4679327d2ed8414b020a893e98
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-06-20 11:13:59 +0200

    Improve codebase and usage.

--- README.md
+++ README.md
@@ -7,25 +7,27 @@ A simple program that issues a command a certain amount of times with a given in
 ## Usage
 
 ```
-autocmd [FLAGS] [OPTIONS] <command> --interval <interval>
+autocmd [FLAGS] [OPTIONS] --interval <interval> [--] <command>...
 ```
 With <command> the command to issue.
 
 Available flags:
-- -h, --help            Prints help information
-- -o, --print_output    Print outputs from the command call
-- -q, --quiet           Disable standard outputs
-- -V, --version         Prints version information
+
+    -h, --help            Prints help information
+    -o, --print_output    Show the chosen command outputs in the standard output (dosen't cancel --quiet)
+    -q, --quiet           Disable AutoCMD standard outputs (doesn't cancel --print_output)
+    -V, --version         Prints version information
 
 Available options:
-- -i, --interval <interval>    Interval between commands in seconds
-- -r, --repeat <repeat>        Repeat n times and stop
+
+    -i, --interval <interval>    Interval between commands in seconds
+    -r, --repeat <repeats>       Repeat <repeats> times and stop
 
 Note that if repeat option is not provided the program shall run indefinitely.
 
 I personally sometimes use it like so:
 ```
-$ autocmd -i 300 "cvlc --play-and-stop --play-and-exit --quiet notif.wav"
+$ autocmd -i 300 -- cvlc --play-and-stop --play-and-exit --quiet notif.wav
 ```
 To indefinitely play a sound every 5 minutes with `cvlc`.
 
@@ -35,5 +37,5 @@ This program is written in [Rust](https://www.rust-lang.org). As such, you can s
 ```
 $ cargo build --release
 ```
-The `autocmd` command shall be found in the folder `./target/release`.
+The `autocmd` program shall be found in the folder `./target/release`.
 
--- src/main.rs
+++ src/main.rs
@@ -44,15 +44,23 @@ macro_rules! eprintln {
     };
 }
 
+fn is_an_unsigned_integer(val: String) -> Result<(), String> {
+    if val.parse::<u64>().is_ok() {
+        Ok(())
+    } else {
+        Err(String::from("the value must be a positive integer."))
+    }
+}
+
 fn main() {
     let matches = clap_app!(AutoCMD =>
         (version: crate_version!())
         (author: crate_authors!())
         (about: "A simple program that issues a command a certain amount of times with a given interval.")
         (after_help: "If repeat option is not provided the program shall run indefinitely.")
-        (@arg command: +required "The command to issue")
-        (@arg interval: +required +takes_value -i --interval "Interval between commands in seconds")
-        (@arg repeat: +takes_value -r --repeat "Repeat <repeat> times and stop")
+        (@arg command: +required +last +multiple "The command to issue")
+        (@arg interval: +required +takes_value {is_an_unsigned_integer} -i --interval "Interval between commands in seconds")
+        (@arg repeats: +takes_value {is_an_unsigned_integer} -r --repeat "Repeat <repeats> times and stop")
         (@arg quiet: -q --quiet "Disable AutoCMD standard outputs (doesn't cancel --print_output)")
         (@arg print_output: -o --print_output "Show the chosen command outputs in the standard output (dosen't cancel --quiet)")
     ).get_matches();
@@ -60,69 +68,92 @@ fn main() {
     let verbose = !matches.is_present("quiet");
     let print_output = matches.is_present("print_output");
 
-    // first unwrap is safe since interval is required
-    let interval_secs = matches.value_of("interval").unwrap().parse::<u64>().unwrap_or_else(|e| {
-        panic!("unable to parse interval as unsigned integer: {}", e)
-    });
+    // here the unwraps are safe since "interval" is required and checked by clap.
+    let interval_secs = matches.value_of("interval").unwrap().parse::<u64>().unwrap();
 
-    let mut remaining_repeats = matches.value_of("repeat").unwrap_or_else(|| {
-        "-1"
-    }).parse::<i64>().unwrap_or_else(|e| {
-        panic!("unable to parse number of repeats as integer: {}", e)
-    });
-
-    let full_command = matches.value_of("command").unwrap(); // safe since command is required
-    let mut command_iter = full_command.split_whitespace();
-    let command_name = command_iter.next().unwrap(); // safe since there is at least one element
-    let mut command = Command::new(command_name);
-    for arg in command_iter {
+    // safe since "command" is required by clap
+    let full_command_str = get_full_command_str_from_values(matches.values_of("command").unwrap());
+    let mut command_and_args_iter = matches.values_of("command").unwrap();
+    let mut command = Command::new(command_and_args_iter.next().unwrap());
+    for arg in command_and_args_iter {
         command.arg(arg);
     }
 
     cprintln!(verbose, "{} {}!\n", "AutoCMD".yellow().bold().italic(), "started".green().bold());
 
     let waiting_duration = time::Duration::from_secs(interval_secs);
-    while remaining_repeats != 0 {
-        // === waiting ===
-        cprintln!(verbose, "Next command in {} seconds.", interval_secs.to_string().green().bold());
-        let now = time::Instant::now();
-        while waiting_duration - now.elapsed() > time::Duration::from_secs(60) {
-            thread::sleep(time::Duration::from_secs(60));
-            cprintln!(verbose, "{} seconds elapsed! {} seconds remaining.",
-                      now.elapsed().as_secs().to_string().green(),
-                      ((waiting_duration - now.elapsed()).as_secs() + 1).to_string().green());
-            //                                                     ^ +1 for rounding
-        }
-        thread::sleep(waiting_duration - now.elapsed());
-        // === waiting finished ===
-
-        // === issue command ===
-        let output = command.output().unwrap_or_else(|e| {
-            panic!("failed to execute process: {}", e)
-        });
-        cprintln!(verbose, "Issued command {}.", full_command.blue().bold());
-
-        if print_output {
-            if output.status.success() {
-                let s = String::from_utf8_lossy(&output.stdout);
-
-                cprintln!(verbose, "Command {}, stdout:", "succeeded".green().bold());
-                println!("{}", s);
-            } else {
-                let s = String::from_utf8_lossy(&output.stderr);
-
-                cprintln!(verbose, "Command {}, stderr:", "failed".red().bold());
-                eprintln!("{}", s);
+    match matches.value_of("repeats") {
+        None => loop {
+            match wait_and_run_once(verbose, waiting_duration, full_command_str.as_str(), &mut command, print_output) {
+                Ok(_) => (),
+                Err(_) => break
             }
-        } else {
-            cprintln!(verbose);
-        }
-        // === command issued ===
+        },
+        Some(repeat_input) => {
+            // here the unwrap is safe since "repeat" is checked by clap.
+            let number_of_repeats = repeat_input.parse::<u64>().unwrap();
+            for remaining_repeats in (0..number_of_repeats).rev() {
+                match wait_and_run_once(verbose, waiting_duration, full_command_str.as_str(), &mut command, print_output) {
+                    Ok(_) => (),
+                    Err(_) => break
+                }
 
-        if remaining_repeats > 0 {
-            remaining_repeats -= 1;
-            cprintln!(verbose, "{} repeats remaining.", remaining_repeats.to_string().yellow().bold());
+                if remaining_repeats > 0 {
+                    cprintln!(verbose, "{} repeats remaining.", remaining_repeats.to_string().yellow().bold());
+                }
+            }
         }
     }
+}
+
+fn get_full_command_str_from_values(values: clap::Values) -> String {
+    let mut vals_vec = Vec::new();
+    for val in values {
+        vals_vec.push(val);
+    }
+    vals_vec.join(" ")
+}
+
+fn wait_and_run_once(verbose: bool, waiting_duration: time::Duration,
+                     full_command_str: &str, command: &mut Command,
+                     print_output: bool) -> Result<(), ()> {
+    // === waiting ===
+    cprintln!(verbose, "Next command in {} seconds.", waiting_duration.as_secs().to_string().green().bold());
+    let now = time::Instant::now();
+    while waiting_duration - now.elapsed() > time::Duration::from_secs(60) {
+        thread::sleep(time::Duration::from_secs(60));
+        cprintln!(verbose, "{} seconds elapsed! {} seconds remaining.",
+                  now.elapsed().as_secs().to_string().green(),
+                  ((waiting_duration - now.elapsed()).as_secs() + 1).to_string().green());
+                                                              // ^ +1 for rounding
+    }
+    thread::sleep(waiting_duration - now.elapsed());
+    // === waiting finished ===
+
+    // === issue command ===
+    let output = match command.output() {
+        Ok(output) => output,
+        Err(e) => {
+            eprintln!("{}: {}\nReason: {}", "Failed to execute".red().bold(), full_command_str.blue().bold(), e);
+            return Err(());
+        }
+    };
+    cprintln!(verbose, "Issued command {}.", full_command_str.blue().bold());
+
+    if print_output {
+        if output.status.success() {
+            cprintln!(verbose, "Command {}:", "succeeded".green().bold());
+        } else {
+            cprintln!(verbose, "Command {}:", "failed".red().bold());
+        }
+
+        print!("{}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+    } else {
+        cprintln!(verbose);
+    }
+    // === command issued ===
+
+    Ok(())
 }
 
